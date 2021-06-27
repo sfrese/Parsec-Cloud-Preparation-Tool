76e616d94755471325ded01ee92ac28429e32113
Sven Frese <svenfrese@gmail.com>
2021-06-27 10:12:33 +0200

    Revert "Test remove the auto logon functionality"
    This reverts commit 4dbd4eaba815de9293bdbf318ae08a007c76cfc2.

--- PostInstall/PostInstall.ps1
+++ PostInstall/PostInstall.ps1
@@ -938,7 +938,7 @@ Write-Host -foregroundcolor red "
                     Google T4  VW    (Tesla T4 Virtual Workstation)
 
 "   
-#PromptUserAutoLogon -DontPromptPasswordUpdateGPU:$DontPromptPasswordUpdateGPU
+PromptUserAutoLogon -DontPromptPasswordUpdateGPU:$DontPromptPasswordUpdateGPU
 $ScripttaskList = @(
 "setupEnvironment";
 "addRegItems";
